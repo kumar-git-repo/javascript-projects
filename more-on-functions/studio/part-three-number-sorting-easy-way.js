a9a3cd9a0dfd998669136d6b863617b871b0d397
Your Name"kamalaGIT <bandhavi.kk@gmail.com>
2024-01-29 20:44:51 -0600

    Studio is completed

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -5,4 +5,27 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
 
+function findMinValue(array) {
+    let minValue = array[0]
+    for (i=1; i<array.length; i++){
+        if (array[i]<minValue){
+            minValue = array[i]
+        } 
+    }
+    return minValue
+}
 //Sort each array in decending order.
+function sortArray(array1) {
+    let sortedArray =[]
+    while (array1.length !==0){
+        let smallValue = findMinValue(array1); 
+        //console.log(smallValue)
+        sortedArray.push(smallValue);
+        //console.log(array1.indexof(smallValue))
+       array1.splice(array1.indexOf(findMinValue(array1)),1);
+        
+    }
+    return sortedArray;
+}
+
+console.log(sortArray([5, 10, 2, 42]))
