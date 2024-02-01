0a12970197f97179a08fe035cd90fabf7385a589
Your Name"kamalaGIT <bandhavi.kk@gmail.com>
2024-02-01 17:35:04 -0600

    Exercise for Objects

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,21 +2,45 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.round(Math.random()*10)}
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.round(Math.random()*10)}
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.round(Math.random()*10)}
+};
 
-// After you have created the other object literals, add the astronautID property to each one.
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.round(Math.random()*10)}
+};
 
-// Create an array to hold the animal objects.
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.round(Math.random()*10)}
+};
 
-// Print out the relevant information about each animal.
-
-// Start an animal race!
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
