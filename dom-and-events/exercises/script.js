689403a1f2b0e0e1a8143df69f3afd68d2ead034
Your Name"kamalaGIT <bandhavi.kk@gmail.com>
2024-02-29 22:48:30 -0600

    Exercise is completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,9 +2,26 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
-    // Put your code for the exercises here.
     
+    // Put your code for the exercises here.
+    button.addEventListener("click", function(){
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+    missionAbort.addEventListener("mouseover", function(){
+       missionAbort.style="background-color: red;";
+    });
+
+    missionAbort.addEventListener("mouseout", function(){
+        missionAbort.style="background-color: '';";
+      
+     }); 
+     missionAbort.addEventListener("click", function(){
+        if(window.confirm("Are you sure you want to abort the mission?"))
+        paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+
+      
+     });      
+
 }
 
 window.addEventListener("load", init);
